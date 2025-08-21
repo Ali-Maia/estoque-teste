1186ee097441c55f09e4f58b96ef6ce6a51de480
Ali-Maia <aliciaengcomp@gmail.com>
2025-08-21 18:55:21 -0300

    feat: add testes edição

---
+++ cypress/e2e/edicao.cy.js
@@ -0,0 +1,64 @@
+describe('Edição de Produtos', () => {
+
+  beforeEach(() => {
+      cy.entrarSite()
+      cy.fixture("produtos").then(produtos => {
+        const produtoCompleto = {
+        name: produtos.valido.name,
+        filamentType: produtos.valido.filamentType,
+        colors: produtos.valido.colors,
+        image: produtos.valido.image,
+        weight: produtos.valido.weight,
+        dimensions: produtos.valido.dimensions,
+        price: produtos.valido.price,
+        quantity: produtos.valido.quantity,
+        description: produtos.valido.description,
+        status: "Disponível"
+      }
+      cy.cadastrarProduto(produtoCompleto)
+      cy.verificarProduto(produtoCompleto)
+
+      })
+  })
+    
+  it('Edição de produto com dados válidos deve permitir editar produto no sistema', () => {
+    cy.fixture("produtos").then(produtos => {
+      const produtoEditado = {
+        name: produtos.valido2.name,
+        filamentType: produtos.valido2.filamentType,
+        colors: produtos.valido2.colors,
+        image: produtos.valido2.image,
+        weight: produtos.valido2.weight,
+        dimensions: produtos.valido2.dimensions,
+        price: produtos.valido2.price,
+        quantity: produtos.valido2.quantity,
+        description: produtos.valido2.description,
+        status: "Disponível"
+      }
+      cy.editarProduto(produtoEditado)
+      cy.verificarProduto(produtoEditado)
+    })
+  })
+  
+    it('Edição de produto com dados válidos deve permitir editar produto no sistema', () => {
+    cy.fixture("produtos").then(produtos => {
+      const produtoOriginal = {name:produtos.valido.name}
+      const produtoEditado = {
+        name: produtos.valido2.name,
+        filamentType: produtos.valido2.filamentType,
+        colors: produtos.valido2.colors,
+        image: produtos.valido2.image,
+        weight: produtos.valido2.weight,
+        dimensions: produtos.valido2.dimensions,
+        price: -1,
+        quantity: produtos.valido2.quantity,
+        description: produtos.valido2.description,
+        status: "Disponível"
+      }
+      cy.editarProduto(produtoEditado)
+      cy.verificarMensagemErro('Preço deve ser um número maior ou igual a zero (R$).')
+      cy.verificarProduto(produtoOriginal)
+    })
+  })
+
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -45,6 +45,50 @@ Cypress.Commands.add('cadastrarProduto', (produto) => {
   cy.get('#submit-button').click();
 });
 
+Cypress.Commands.add('editarProduto', (produto) => {
+  cy.get('[data-action="edit"]').click()
+  cy.contains('#form-title','Editar Produto').should('be.visible')
+  // Verifica se o campo 'name' foi fornecido antes de preencher
+  if (produto.name) {
+    cy.get('[name="name"]').click().clear().type(produto.name);
+  }
+
+  if (produto.filamentType) {
+    cy.get('[name="filamentType"]').click().clear().type(produto.filamentType);
+  }
+
+  if (produto.colors) {
+    cy.get('[name="colors"]').click().clear().type(produto.colors);
+  }
+
+  if (produto.image) {
+ 
+    cy.get('[name="image"]').selectFile(produto.image, { force: true });
+  }
+
+  if (produto.weight) {
+    cy.get('[name="weight"]').click().clear().type(produto.weight);
+  }
+
+  if (produto.dimensions) {
+    cy.get('[name="dimensions"]').click().clear().type(produto.dimensions);
+  }
+
+  if (produto.price) {
+    cy.get('[name="price"]').click().clear().type(produto.price);
+  }
+
+  if (produto.quantity) {
+    cy.get('[name="quantity"]').click().clear().type(produto.quantity);
+  }
+
+  if (produto.description) {
+    cy.get('[name="description"]').click().clear().type(produto.description);
+  }
+
+  cy.get('#submit-button').click();
+});
+
 Cypress.Commands.add('verificarMensagemErro', (mensagem) => {
   cy.contains('#form-hint', mensagem).should('be.visible')
 })
@@ -57,4 +101,8 @@ Cypress.Commands.add('verificarProduto', (produto) => {
   if(produto.status){
     cy.contains('.status', produto.status).should('be.visible')
   }
+})
+
+Cypress.Commands.add('excluirProduto', () => {
+  cy.get('.btn-danger').click()
 })
